f08b625b1bc6ea8302305fe26960c7e9faf68e5c
bdadam <me@bdadam.com>
2017-06-25 23:58:47 +0200

    grid example

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,33 @@ import { h } from 'preact';
 
 export default () => {
     return (
-        <div class="container">Home home</div>
+        <div class="container">
+            <h1>Home home</h1>
+
+            <div class="row">
+                <div class="col" style="background-color: #aaa; height: 48px;"></div>
+                <div class="col" style="background-color: #aaa; height: 48px;"></div>
+                <div class="col" style="background-color: #aaa; height: 48px;"></div>
+                <div class="col" style="background-color: #aaa; height: 48px;"></div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col" style="background-color: #aaa; height: 48px;"></div>
+                <div class="col" style="background-color: #aaa; height: 48px;"></div>
+                <div class="col" style="background-color: #aaa; height: 48px;"></div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-sm-2" style="background-color: #aaa; height: 48px;"></div>
+                <div class="col-sm-3" style="background-color: #aaa; height: 48px;"></div>
+                <div class="col-sm-4" style="background-color: #aaa; height: 48px;"></div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-lg-2" style="background-color: #aaa; height: 48px;"></div>
+                <div class="col-lg-3" style="background-color: #aaa; height: 48px;"></div>
+                <div class="col-lg-4" style="background-color: #aaa; height: 48px;"></div>
+            </div>
+        </div>
     );
 };
