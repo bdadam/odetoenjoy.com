0dde0e47e5019598b4c7ec513ec6850d0c5eefe1
bdadam <me@bdadam.com>
2017-07-03 01:38:08 +0200

    using jimp instead of gm

--- src/services/image-service.js
+++ src/services/image-service.js
@@ -1,13 +1,9 @@
 import crypto from 'crypto';
 
-import gm from 'gm';
-import request from 'request';
-
+import Jimp from 'jimp';
 import uuid from 'uuid/v4';
 
 import Image from '../models/image-model';
-
-import Jimp from 'jimp';
 
 export const createPreviewFromUrl = (url) => new Promise((resolve, reject) => {
 
@@ -29,31 +25,6 @@ export const createPreviewFromUrl = (url) => new Promise((resolve, reject) => {
             }).then((img) => {
                 resolve(img.id);
             });
-
         });
     });
-
-    // gm(request(url), 'preview.jpg')
-    //     .autoOrient()
-    //     .noProfile()
-    //     .resize(640, 480)
-    //     .quality(85)
-    //     .toBuffer('JPG', (err, buffer) => {
-    //         if (err) { return reject(err); }
-    //         gm(buffer)
-    //             .size((err, { width, height }) => {
-    //                 if (err) { return reject(err); }
-
-    //                 Image.create({
-    //                     id: uuid(),
-    //                     md5: crypto.createHash('md5').update(buffer).digest('hex'),
-    //                     width,
-    //                     height,
-    //                     from: url,
-    //                     image: buffer
-    //                 }).then((img) => {
-    //                     resolve(img.id);
-    //                 });
-    //             });          
-    //     });
 });
