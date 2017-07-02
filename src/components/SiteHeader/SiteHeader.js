85833d5e19a6b44257b29be08442c5de4ef8018d
bdadam <me@bdadam.com>
2017-07-02 02:48:27 +0200

    links and color in header

--- src/components/SiteHeader/SiteHeader.js
+++ src/components/SiteHeader/SiteHeader.js
@@ -8,6 +8,7 @@ export default () => {
             <nav class="container">
                 <ul class="row">
                     <li class="col-auto"><a href="/" class="site-header__link">Home</a></li>
+                    <li class="col-auto"><a href="/videos" class="site-header__link">Videos</a></li>
                     <li class="col-auto"><a href="/upload" class="site-header__link">Upload</a></li>
                     <li class="col-auto"><a class="site-header__link" href="/login">Login</a></li>
                 </ul>
