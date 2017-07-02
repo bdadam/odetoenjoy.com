c48772fe60f47d7d29ac2e34d3aa8f27c5c77f29
bdadam <me@bdadam.com>
2017-07-02 02:47:43 +0200

    Hero image

--- src/server.js
+++ src/server.js
@@ -35,6 +35,7 @@ const Html = ({ head, scriptUrl, cssUrl, children }) => {
 
 // app.use('/static', express.static(path.resolve(process.cwd(), 'dist'), { maxAge: '30 days', index: false, etag: false }));
 app.use('/static', express.static(path.resolve(process.cwd(), 'dist'), { index: false, etag: false }));
+app.use('/images', express.static(path.resolve(process.cwd(), 'static/images'), { index: false, etag: false }));
 
 import gqlRouter from './server-api';
 
