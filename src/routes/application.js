c516b674abbf92215f89f746012365faa913f23b
bdadam <me@bdadam.com>
2017-07-24 00:34:04 +0200

    eslint

--- src/routes/application.js
+++ src/routes/application.js
@@ -7,18 +7,18 @@ import Application from '../Application';
 
 const Html = ({ scriptUrl, cssUrl, children }) =>
     <html lang="en">
-            <head>
-                <meta charset="utf-8" />
+        <head>
+            <meta charset="utf-8" />
             <title>Ode to enjoy</title>
             <meta name="description" property="og:description" content="descriptiojn" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="stylesheet" href={cssUrl} />
-            </head>
-            <body>
-                <div id="app">{children}</div>
-                <script src={scriptUrl} defer></script>
-                <script src="http://localhost:35729/livereload.js?snipver=1" async></script>
-            </body>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="stylesheet" href={cssUrl} />
+        </head>
+        <body>
+            <div id="app">{children}</div>
+            <script src={scriptUrl} defer></script>
+            <script src="http://localhost:35729/livereload.js?snipver=1" async></script>
+        </body>
     </html>;
 
 const router = Router();
