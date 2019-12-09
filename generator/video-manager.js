0865f136748222f130d08f0a38284a024c9f1f9a
Adam <me@bdadam.com>
2019-12-10 00:33:41 +0100

    try-catch

--- generator/video-manager.js
+++ generator/video-manager.js
@@ -13,7 +13,6 @@ const Datastore = require('nedb');
 const dbpath = path.resolve(process.cwd(), 'content/videos.nedb');
 const videodb = new Datastore({ filename: dbpath, autoload: true });
 
-
 // const loadHtml = bent('string', { 'User-Agent': 'Googlebot-Video/1.0' });
 // const loadHtml = bent('string', { 'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' });
 const loadHtml = bent('string');
@@ -35,50 +34,68 @@ const formatDuration = (hours, minutes, seconds) => {
     return `${minutes}:${String(seconds).padStart(2, '0')}`;
 };
 
-const crawlVideoPage = async (videoUrl) => {
-    const html = await loadHtml(videoUrl);
-    const $ = cheerio.load(html);
+const crawlVideoPage = async videoUrl => {
+    try {
+        const html = await loadHtml(videoUrl);
+        const $ = cheerio.load(html);
 
-    // const rawCanonical = $('link[rel=canonical]').attr('href');
-    // const canonical = URL.resolve(videoUrl, rawCanonical);
-    // const embedUrl = $('');
+        // const rawCanonical = $('link[rel=canonical]').attr('href');
+        // const canonical = URL.resolve(videoUrl, rawCanonical);
+        // const embedUrl = $('');
 
-    const embedUrl = $('meta[property="og:video:url"]').attr('content');
-    const imageUrl = $('meta[property="og:image"]').attr('content');
-    const durationStr = $('meta[itemprop="duration"]').attr('content');
-    
-    const duration = moment.duration(durationStr);
+        const embedUrl = $('meta[property="og:video:url"]').attr('content');
+        const imageUrl = $('meta[property="og:image"]').attr('content');
+        const durationStr = $('meta[itemprop="duration"]').attr('content');
 
-    const formattedDuration = formatDuration(duration.hours(), duration.minutes(), duration.seconds());
-    const image = await loadBinary(imageUrl);
-    const imageFileName = crypto.createHash('md5').update(image).digest('hex');
+        const duration = moment.duration(durationStr);
 
-    const x = await jimp.read(image);
-    const thumbnail = await new Promise(resolve => x.background(0x0000000000).contain(480, 270).quality(80).getBuffer('image/jpeg', (err, buf) => resolve(buf)));
-    const thumbnailFileName = crypto.createHash('md5').update(thumbnail).digest('hex');
+        const formattedDuration = formatDuration(duration.hours(), duration.minutes(), duration.seconds());
+        const image = await loadBinary(imageUrl);
+        const imageFileName = crypto
+            .createHash('md5')
+            .update(image)
+            .digest('hex');
 
-    fs.writeFileSync(`dist/video-images/${imageFileName}.jpg`, image);
-    fs.writeFileSync(`dist/video-images/${thumbnailFileName}.jpg`, thumbnail);
+        const x = await jimp.read(image);
+        const thumbnail = await new Promise(resolve =>
+            x
+                .background(0x0000000000)
+                .contain(480, 270)
+                .quality(80)
+                .getBuffer('image/jpeg', (err, buf) => resolve(buf))
+        );
+        const thumbnailFileName = crypto
+            .createHash('md5')
+            .update(thumbnail)
+            .digest('hex');
 
-    return {
-        image: `/video-images/${imageFileName}.jpg`,
-        thumbnail: `/video-images/${thumbnailFileName}.jpg`,
-        duration: formattedDuration,
-        embedUrl
-    };
+        fs.writeFileSync(`dist/video-images/${imageFileName}.jpg`, image);
+        fs.writeFileSync(`dist/video-images/${thumbnailFileName}.jpg`, thumbnail);
+
+        return {
+            image: `/video-images/${imageFileName}.jpg`,
+            thumbnail: `/video-images/${thumbnailFileName}.jpg`,
+            duration: formattedDuration,
+            embedUrl,
+        };
+    } catch (e) {
+        console.error('ERROR:', videoUrl);
+    }
 };
 
-Promise.all(videos.map(async (video, index) => {
-    const { image, duration, thumbnail, embedUrl } = await crawlVideoPage(video.src);
-    video._id = video.slug || speakingurl(video.title, { lang: 'en' });
-    video.slug = video._id;
-    video.duration = duration;
-    video.image = image;
-    video.thumbnail = thumbnail;
-    video.index = index;
-    video.embedUrl = embedUrl;
+Promise.all(
+    videos.map(async (video, index) => {
+        const { image, duration, thumbnail, embedUrl } = await crawlVideoPage(video.src);
+        video._id = video.slug || speakingurl(video.title, { lang: 'en' });
+        video.slug = video._id;
+        video.duration = duration;
+        video.image = image;
+        video.thumbnail = thumbnail;
+        video.index = index;
+        video.embedUrl = embedUrl;
 
-    videodb.update({ _id: video._id }, video, { upsert: true });
-})).then(() => {
+        videodb.update({ _id: video._id }, video, { upsert: true });
+    })
+).then(() => {
     videodb.persistence.compactDatafile();
 });
